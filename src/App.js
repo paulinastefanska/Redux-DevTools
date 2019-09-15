d44174cab0115fb990e7de6be9c01c24c37082ed
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-15 12:35:41 +0200

    Import devtools and add to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CommentsListContainer from './CommentsListContainer';
 import NewCommentContainer from './NewCommentContainer';
+import DevTools from './DevTools';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <h1>Comments App</h1>
       <CommentsListContainer />
       <NewCommentContainer />
+      <DevTools />
     </div>
   );
 };
